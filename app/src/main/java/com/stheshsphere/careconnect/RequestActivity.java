c9749370c684fccf874f80ab56a2d15ad748ce1d
StheshSphere <stheshsphere@gmail.com>
2025-09-03 20:23:43 +0200

    Commiting Designs

--- app/src/main/java/com/stheshsphere/careconnect/RequestActivity.java
+++ app/src/main/java/com/stheshsphere/careconnect/RequestActivity.java
@@ -1,4 +1,13 @@
 package com.stheshsphere.careconnect;
 
-public class RequestActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request); // Make sure you have this layout file
+    }
 }
